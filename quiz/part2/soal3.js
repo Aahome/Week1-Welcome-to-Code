cf7f7952fa129e883682f8faf28afc6ec171ce80
Aahome <azizhanxk@gmail.com>
2025-02-27 09:46:30 +0700

    push part 1 n part 2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,17 @@
+// ## Soal 3. Breaking Sentence (Again) using Substring
+// js
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let FirstWord3 = word3.substring(0, 3);
+let SecondWord2 = word3.substring(4, 14);
+let ThirdWord3 = word3.substring(15, 17);
+let FourthWord4 = word3.substring(18, 20);
+let FifthWord5 = word3.substring(21, 25);
+console.log('First Word: ' + FirstWord3);
+console.log('Second Word: ' + SecondWord2);
+console.log('Third Word: ' + ThirdWord3);
+console.log('Fourth Word: ' + FourthWord4);
+console.log('Fifth Word: ' + FifthWord5);
+// Output
